3a9d956efcb768564766d77345add881d0daf3dd
chirchir <chirchir7370@gmail.com>
2022-09-21 20:09:14 +0300

    validate createUser api

--- src/users/dtos/index.dto.ts
+++ src/users/dtos/index.dto.ts
@@ -1,7 +1,21 @@
+import { IsNotEmpty, Max, Min, IsEmail, IsPhoneNumber } from 'class-validator';
 export class UserDto {
+  @IsNotEmpty()
   readonly firstName: string;
+
+  @IsNotEmpty()
   readonly lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @Max(50)
+  @Min(7)
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
   readonly phoneNumber: string;
 }
