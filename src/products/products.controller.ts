2727ee2a5158282c14b024c7dbe3c52c165a6146
chirchir <chirchir7370@gmail.com>
2022-09-21 23:50:28 +0300

    product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductDto } from './dtos/product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Post('new')
+  async create(@Body() product: ProductDto) {
+    return this.productService.create(product);
+  }
+
+  @Get('show')
+  async show() {
+    return this.productService.show();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() product: ProductDto) {
+    return this.productService.update(id, product);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.productService.delete(id);
+  }
+}
