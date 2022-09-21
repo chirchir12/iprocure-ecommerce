1977019b249e673a8d36749d92bcd48a0f084731
chirchir <chirchir7370@gmail.com>
2022-09-21 23:49:50 +0300

    product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { PRODUCT_REPOSITORY } from './constants';
+import { ProductDto } from './dtos/product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @Inject(PRODUCT_REPOSITORY)
+    private readonly productRepository: typeof Product,
+  ) {}
+
+  async create(product: ProductDto): Promise<ProductDto> {
+    return await this.productRepository.create(product);
+  }
+
+  async show(): Promise<Product[]> {
+    return await this.productRepository.findAll();
+  }
+
+  async update(id, product: ProductDto): Promise<Product> {
+    const roleExist = await this.productRepository.findOne({ where: { id } });
+    if (!roleExist) {
+      throw new NotFoundException('Product does not exist');
+    }
+    const [numberOfAffectedRows, [updatedProduct]] =
+      await this.productRepository.update(
+        { ...product },
+        { where: { id }, returning: true },
+      );
+    const { ...results } = updatedProduct['dataValues'];
+    return results;
+  }
+
+  async delete(id: number) {
+    const roleExist = await this.productRepository.findOne({ where: { id } });
+    if (!roleExist) {
+      throw new NotFoundException('Product does not exist');
+    }
+    return this.productRepository.destroy({ where: { id } });
+  }
+}
