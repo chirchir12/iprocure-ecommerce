58fb2578e65b0def2fa5f798fc15a966a3be3e13
chirchir <chirchir7370@gmail.com>
2022-09-21 19:45:17 +0300

    enable global input validations

---
+++ src/core/pipes/validate.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateInputPipe extends ValidationPipe {
+  public async transform(value, metadata: ArgumentMetadata) {
+    try {
+      return await super.transform(value, metadata);
+    } catch (e) {
+      if (e instanceof BadRequestException) {
+        throw new UnprocessableEntityException(this.handleError(e.message));
+      }
+      throw e;
+    }
+  }
+
+  private handleError(errors: any) {
+    return errors.map((error: any) => error.constraints);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // set global prefix
   app.setGlobalPrefix('api/v1');
+
+  // autovalidate
+  app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(3000);
 }
 bootstrap();
