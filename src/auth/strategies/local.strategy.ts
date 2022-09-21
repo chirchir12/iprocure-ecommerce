1251b262bc81ace0f5fa047babb98088666bf9fb
chirchir <chirchir7370@gmail.com>
2022-09-21 18:53:46 +0300

    local strategy - login with username and password

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super(); // no config needed for local strategy
+  }
+  // passport expects this method with the followeing signature
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException('invalid user credentials');
+    }
+    return user; // password will add this to the Request
+  }
+}
