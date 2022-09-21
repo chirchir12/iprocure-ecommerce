20c93aefbd691ce0b462c726cb88963e11b2d5eb
chirchir <chirchir7370@gmail.com>
2022-09-21 18:55:56 +0300

    include passport modules for jwt and local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JWTStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWTKEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRES },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JWTStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
