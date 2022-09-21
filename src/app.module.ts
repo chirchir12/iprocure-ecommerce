17cb6268a415095c80a2fc9f5532826a387c12a1
chirchir <chirchir7370@gmail.com>
2022-09-21 23:11:14 +0300

    products module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { RolesModule } from './roles/roles.module';
     UsersModule,
     AuthModule,
     RolesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
