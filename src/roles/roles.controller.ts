c421c8528f1f7f22fee76884fa2873821d8f12f9
chirchir <chirchir7370@gmail.com>
2022-09-21 22:58:29 +0300

    role controller

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { RoleDto } from './dtos/role.dto';
+import { RolesService } from './roles.service';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(private readonly roleService: RolesService) {}
+
+  @Post('new')
+  async create(@Body() role: RoleDto) {
+    return this.roleService.create(role);
+  }
+
+  @Get('show')
+  async show() {
+    return this.roleService.show();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() role: RoleDto) {
+    return this.roleService.update(id, role);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.roleService.delete(id);
+  }
+}
