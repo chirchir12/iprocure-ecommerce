41edf5bc37725ab40577a58bf0caa90535ef12cd
chirchir <chirchir7370@gmail.com>
2022-09-21 22:15:51 +0300

    add users controller to the user module class

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { usersProviders } from './prividers/user.providers';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   providers: [UsersService, ...usersProviders],
-  exports: [UsersService], // we will use this outside of this module
+  exports: [UsersService],
+  controllers: [UsersController], // we will use this outside of this module
 })
 export class UsersModule {}
