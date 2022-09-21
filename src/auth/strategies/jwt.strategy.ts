cc7ff22b463d58250b28aa11b36205b1229f92e3
chirchir <chirchir7370@gmail.com>
2022-09-21 18:53:17 +0300

    jwt strategy for generating access token

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWTKEY,
+    });
+  }
+
+  async validate(payload: any) {
+    // check user exist
+    const user = await this.userService.findOneById(payload.id);
+    if (!user) {
+      throw new UnauthorizedException('Forbidden');
+    }
+    return payload;
+  }
+}
