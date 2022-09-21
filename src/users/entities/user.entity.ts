39ac9a4694c2cb1b89254f5af0859d8ee63b4f15
chirchir <chirchir7370@gmail.com>
2022-09-21 14:48:46 +0300

    add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  firstName: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  lastName: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.NUMBER,
+    allowNull: false,
+  })
+  phoneNumber: number;
+}
