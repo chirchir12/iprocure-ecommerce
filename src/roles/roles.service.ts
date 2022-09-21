7985a3ca2bccd938f4499f01fa86429e0ede7f7b
chirchir <chirchir7370@gmail.com>
2022-09-21 22:58:05 +0300

    role service

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ROLES_REPOSITORY } from './constants';
+import { RoleDto } from './dtos/role.dto';
+import { Role } from './entities/role.entity';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(
+    @Inject(ROLES_REPOSITORY) private readonly roleRepository: typeof Role,
+  ) {}
+
+  async create(role: RoleDto): Promise<Role> {
+    return await this.roleRepository.create(role);
+  }
+
+  async show(): Promise<Role[]> {
+    return await this.roleRepository.findAll();
+  }
+
+  async update(id, role: RoleDto): Promise<Role> {
+    const roleExist = await this.roleRepository.findOne({ where: { id } });
+    if (!roleExist) {
+      throw new NotFoundException('role not found');
+    }
+    const [numberOfAffectedRows, [updatedRole]] =
+      await this.roleRepository.update(
+        { ...role },
+        { where: { id }, returning: true },
+      );
+    const { password, ...results } = updatedRole['dataValues'];
+    return results;
+  }
+
+  async delete(id: number) {
+    const roleExist = await this.roleRepository.findOne({ where: { id } });
+    if (!roleExist) {
+      throw new NotFoundException('Role does not exist');
+    }
+    return this.roleRepository.destroy({ where: { id } });
+  }
+}
